1fee3309483f4f4151417ce9f34f450da9629559
Yan Zhuk <yan.zhuk45@gmail.com>
2018-02-25 11:47:05 -0500

    add nav

--- file-selecting/index.php
+++ file-selecting/index.php
@@ -29,7 +29,31 @@
     </style>
 </head>
 
+
+
 <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">SkinGen</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Genetics <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#"></a>Testing</li>
+              <li><a href="#">Results</a></li>
+              <li><a href="#">Skin</a></li>
+            </ul>
+          </li>
+          <li><a href="#">About</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </nav>
 
     <div id="personContainer">
         <?php print($access_token) ?> dsfdsfdsasdfdsfdsdsf
